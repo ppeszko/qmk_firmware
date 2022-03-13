d8895118f913df07d776a5726a119c5c6e6edab0
Patryk Peszko <me@patrykpeszko.com>
2022-03-13 18:16:43 +0100

    add 1password shortcut

--- keyboards/ferris/keymaps/ppeszko/keymap.c
+++ keyboards/ferris/keymaps/ppeszko/keymap.c
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //---------------------------------------------------------------------------------------     ----------------------------------------------------------------------------------
     KC_ESCAPE,  KC_AT,  LSFT(KC_3),         TD(DANCE_31),               KC_PERC,                KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_SCOLON,  KC_BSPC ,
   //---------------------------------------------------------------------------------------     ----------------------------------------------------------------------------------
-    KC_TAB,     KC_EQL, LCA(KC_LEFT),   LCA(KC_RIGHT),    TD(DANCE_32),           TD(BKSL_HME),   LSFT(KC_LBRC),  LSFT(KC_RBRC),  TD(PIPE_END),   KC_ENTER,
+    KC_TAB,     KC_EQL, LCA(KC_LEFT),   LCA(KC_RIGHT),    TD(DANCE_32),           TD(BKSL_HME), LSFT(KC_LBRC),  LSFT(KC_RBRC),  TD(PIPE_END),   KC_ENTER,
   //---------------------------------------------------------------------------------------     ----------------------------------------------------------------------------------
-    KC_TILD,    TD(DANCE_33),   LALT(KC_LBRACKET),  LALT(LSFT(KC_LBRACKET)),    KC_DQUO,        KC_LBRC     ,KC_LPRN   ,KC_RPRN   ,KC_RBRC   ,TO(3)   ,
+    KC_TILD,    TD(DANCE_33),   LALT(KC_LBRACKET),  LAG(KC_BACKSLASH),    KC_DQUO,              KC_LBRC     ,KC_LPRN   ,KC_RPRN   ,KC_RBRC   ,TO(3)   ,
   //---------------------------------------------------------------------------------------     ----------------------------------------------------------------------------------
 		    					TO(0),  OSM(MOD_LSFT),                                          KC_RALT,    TO(2) 
   ),
